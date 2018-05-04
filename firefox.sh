3a604a784f2ffef79fbda9f7897fd60f0775ceae
Martin John <20387863+martingjohn@users.noreply.github.com>
2018-05-04 15:55:02 +0100

    added home directories for firefox

--- firefox.sh
+++ firefox.sh
@@ -1,14 +1,15 @@
 docker run \
 	--rm \
-	--name=firefox \
-	-d \
 	--net host \
+	-d \
 	-e DISPLAY=unix$DISPLAY \
 	-e PULSE_SERVER=$PULSE_SERVER \
 	-e PULSE_COOKIE=/home/ubuntu/.config/pulse/cookie \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /home/${USER}/.config/pulse/cookie:/home/ubuntu/.config/pulse/cookie \
-	-v /dev/shm:/dev/shm \
+        -v ${HOME}/.cache:/home/ubuntu/.cache \
+        -v ${HOME}/.mozilla:/home/ubuntu/.mozilla \
 	-v /etc/localtime:/etc/localtime:ro \
+	-v /dev/shm:/dev/shm \
 	--device /dev/dri \
 	firefox $*
